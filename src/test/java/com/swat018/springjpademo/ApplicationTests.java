f6b522e609c1abbdda8613f83e383c65f4968e2a
jinwoo park <swat018@gmail.com>
2020-06-26 19:55:41 +0900

    Initial commit

---
+++ src/test/java/com/swat018/springjpademo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.springjpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
